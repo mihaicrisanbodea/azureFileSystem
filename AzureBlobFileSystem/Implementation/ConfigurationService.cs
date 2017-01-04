aad6ccea7d4cd3596618854b1efa2c7bcd2aa59e
Mihai Crisan Bodea <mihaicrisanbodea@yahoo.com>
2017-01-04 21:47:21 +0200

    update package info

--- AzureBlobFileSystem/Implementation/ConfigurationService.cs
+++ AzureBlobFileSystem/Implementation/ConfigurationService.cs
@@ -5,15 +5,29 @@ namespace AzureBlobFileSystem.Implementation
 {
     public class ConfigurationService : IConfigurationService
     {
+        private const int DefaultBlobListingPageSize = 500;
+
         public string StorageAccountConnectionString
             => ConfigurationManager.AppSettings["StorageAccountConnectionString"];
 
         public string ContainerName => ConfigurationManager.AppSettings["ContainerName"];
 
-        public string DefaultFileName => "temp.tmp";
+        public string DefaultFileName => ConfigurationManager.AppSettings["DefaultFileName"];
 
-        public string UtcTimeFormat => "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'";
+        public string UtcTimeFormat => ConfigurationManager.AppSettings["UtcTimeFormat"];
 
-        public int BlobListingPageSize => 500;
+        public int BlobListingPageSize => GetBlobListingPageSize();
+
+        private int GetBlobListingPageSize()
+        {
+            var pageSizeString = ConfigurationManager.AppSettings["BlobListingPageSize"];
+            int pageSize;
+            if (int.TryParse(pageSizeString, out pageSize))
+            {
+                return pageSize;
+            }
+
+            return DefaultBlobListingPageSize;
+        }
     }
 }
